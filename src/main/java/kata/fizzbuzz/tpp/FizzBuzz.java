003b8e97d0b0c392d4dd1957cb3e14a9ac4b3172
somkiat <somkiat.p@gmail.com>
2014-10-29 22:54:03 +0700

    Refactoring code to extract Rules to new file

--- src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
@@ -2,34 +2,6 @@ package kata.fizzbuzz.tpp;
 
 public class FizzBuzz {
 
-	enum Rules {
-		Fizz {
-			public String getResult(int number, String result) {
-				return result + (isFizz(number) ? "Fizz" : "");
-			}
-
-			private boolean isFizz(int number) {
-				return number % 3 == 0;
-			}
-		},
-		Buzz {
-			public String getResult(int number, String result) {
-				return result + (isBuzz(number) ? "Buzz" : "");
-			}
-
-			private boolean isBuzz(int number) {
-				return number % 5 == 0;
-			}
-		},
-		Other {
-			public String getResult(int number, String result) {
-				return (result.isEmpty()) ? String.valueOf(number) : result;
-			}
-
-		};
-		public abstract String getResult(int number, String result);
-	}
-
 	public static String say(int number) {
 		String result = "";
 		for(Rules rule : Rules.values()) {
---
+++ src/main/java/kata/fizzbuzz/tpp/Rules.java
@@ -0,0 +1,29 @@
+package kata.fizzbuzz.tpp;
+
+enum Rules {
+	Fizz {
+		public String getResult(int number, String result) {
+			return result + (isFizz(number) ? "Fizz" : "");
+		}
+
+		private boolean isFizz(int number) {
+			return number % 3 == 0;
+		}
+	},
+	Buzz {
+		public String getResult(int number, String result) {
+			return result + (isBuzz(number) ? "Buzz" : "");
+		}
+
+		private boolean isBuzz(int number) {
+			return number % 5 == 0;
+		}
+	},
+	Other {
+		public String getResult(int number, String result) {
+			return (result.isEmpty()) ? String.valueOf(number) : result;
+		}
+
+	};
+	public abstract String getResult(int number, String result);
+}
