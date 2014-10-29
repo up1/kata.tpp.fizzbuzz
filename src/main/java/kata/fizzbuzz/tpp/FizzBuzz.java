0b2b21dae2e8d4c30de2669d615815d684810d61
somkiat <somkiat.p@gmail.com>
2014-10-29 22:51:05 +0700

    Transformation code with variable -> array and array->container/collection

--- src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
@@ -2,32 +2,40 @@ package kata.fizzbuzz.tpp;
 
 public class FizzBuzz {
 
+	enum Rules {
+		Fizz {
+			public String getResult(int number, String result) {
+				return result + (isFizz(number) ? "Fizz" : "");
+			}
+
+			private boolean isFizz(int number) {
+				return number % 3 == 0;
+			}
+		},
+		Buzz {
+			public String getResult(int number, String result) {
+				return result + (isBuzz(number) ? "Buzz" : "");
+			}
+
+			private boolean isBuzz(int number) {
+				return number % 5 == 0;
+			}
+		},
+		Other {
+			public String getResult(int number, String result) {
+				return (result.isEmpty()) ? String.valueOf(number) : result;
+			}
+
+		};
+		public abstract String getResult(int number, String result);
+	}
+
 	public static String say(int number) {
 		String result = "";
-		result = fizzy(number, result);
-		result = buzzy(number, result);
-		result = other(number, result);
+		result = Rules.Fizz.getResult(number, result);
+		result = Rules.Buzz.getResult(number, result);
+		result = Rules.Other.getResult(number, result);
 		return result;
-	}
-
-	private static String other(int number, String result) {
-		return ( result.isEmpty() ? String.valueOf(number) : result );
-	}
-
-	private static String buzzy(int number, String result) {
-		return result + ( isBuzz(number) ? "Buzz" : "" );
-	}
-
-	private static String fizzy(int number, String result) {
-		return result + ( isFizz(number) ? "Fizz" : "" );
-	}
-
-	private static boolean isBuzz(int number) {
-		return number % 5 == 0;
-	}
-
-	private static boolean isFizz(int number) {
-		return number % 3 == 0;
 	}
 
 }
