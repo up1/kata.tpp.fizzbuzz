734d38aec433463781948c50423e920706c7162e
somkiat <somkiat.p@gmail.com>
2014-10-29 22:14:30 +0700

    Refactoring code to extract condition to new method

--- src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
@@ -4,13 +4,21 @@ package kata.fizzbuzz.tpp;
 public class FizzBuzz {
 
 	public static String say(int number) {
-		if(number % 3 == 0 && number % 5 == 0)
+		if(isFizz(number) && isBuzz(number))
 			return "FizzBuzz";
-		if(number % 3 == 0)
+		if(isFizz(number))
 			return "Fizz";
-		if(number % 5 == 0)
+		if(isBuzz(number))
 			return "Buzz";
 		return String.valueOf(number);
+	}
+
+	private static boolean isBuzz(int number) {
+		return number % 5 == 0;
+	}
+
+	private static boolean isFizz(int number) {
+		return number % 3 == 0;
 	}
 
 }
