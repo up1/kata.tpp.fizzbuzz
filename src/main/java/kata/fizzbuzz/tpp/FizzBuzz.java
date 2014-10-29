71745bd4db4b133a470c96354e38bbd7447aa17f
somkiat <somkiat.p@gmail.com>
2014-10-29 22:12:32 +0700

    Transformation with add new case/if to passed test case

--- src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
@@ -4,6 +4,8 @@ package kata.fizzbuzz.tpp;
 public class FizzBuzz {
 
 	public static String say(int number) {
+		if(number % 3 == 0 && number % 5 == 0)
+			return "FizzBuzz";
 		if(number % 3 == 0)
 			return "Fizz";
 		if(number % 5 == 0)
