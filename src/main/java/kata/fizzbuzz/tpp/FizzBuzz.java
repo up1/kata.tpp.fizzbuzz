5e723bb2fb042dc780777ab2a0fa5fddb56f9c7c
somkiat <somkiat.p@gmail.com>
2014-10-29 22:06:26 +0700

    Resolve test case by change condition

--- src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
@@ -4,7 +4,7 @@ package kata.fizzbuzz.tpp;
 public class FizzBuzz {
 
 	public static String say(int number) {
-		if(number == 3)
+		if(number % 3 == 0)
 			return "Fizz";
 		return String.valueOf(number);
 	}
