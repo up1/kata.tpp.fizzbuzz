54c3d9b80102720518be0b45d37d77e9c5042708
somkiat <somkiat.p@gmail.com>
2014-10-31 00:22:44 +0700

    Rewrite code with SRP principle + constant->variable, and variable->array

--- src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
+++ src/main/java/kata/fizzbuzz/tpp/FizzBuzz.java
@@ -4,12 +4,23 @@ public class FizzBuzz {
 
 	public static final String EMPTY_STRING = "";
 
+	static int[] firstNumber = { 3, 5, 15, 7 };
+	static String[] firstResult = { "Fizz", "Buzz", "FizzBuzz", "ByeBye" };
+
 	public static String say(int number) {
-		String result = EMPTY_STRING;
-		for(Rules rule : Rules.values()) {
-			result = rule.getResult(number, result);
+		String result = String.valueOf(number);
+
+		for (int i = 0; i < firstNumber.length; i++) {
+			if (isValid(number, i)) {
+				result = firstResult[i];
+			}
 		}
+
 		return result;
+	}
+
+	private static boolean isValid(int number, int i) {
+		return number % firstNumber[i] == 0;
 	}
 
 }
